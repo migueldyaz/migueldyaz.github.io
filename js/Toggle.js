c07f04934c17767ac40c5efad5d2bcc554ab4d8b
Erik Miguel Diaz Salazar <mds.sith@gmail.com>
2021-04-05 15:10:28 -0500

    Update style 1
    Update style and events.

---
+++ js/Toggle.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#myBOTTON1").click(function(){
+    $("#myTAB1").fadeToggle("slow");
+  });
+
+  $("#myBOTTON2").click(function(){
+    $("#myTAB2").fadeToggle("slow");
+  });
+
+  $("#myBOTTON3").click(function(){
+    $("#myTAB3").fadeToggle("slow");
+  });
+});
